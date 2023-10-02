a70a6ac774739b4efdcd51f5d02cef211c2bcb64
lovepreet <klovepreet12199@gmail.com>
2023-10-01 21:22:32 -0400

    let's manipulate the DOM with useRef

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useRef } from "react";
+import React from 'react';
 import './App.css';
 
 function App() {
+  const inputRef = useRef();
+  const inputRef2 = useRef();
+  const focusInput = () => 
+  {inputRef.current.focus();
+   inputRef.current.style.width = "300px";
+  }
+  const focusInput2 = () => 
+  {inputRef2.current.focus();
+   inputRef2.current.style.width = "300px";
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> The use of useRef </h2>
+      <input type="text" style= {{width:"100px"}} ref={inputRef} />
+      <input type="text" style= {{width:"100px"}} ref={inputRef2}/>
+      <button onClick={focusInput}> Focus 1 </button>
+      <button onClick={focusInput2}> Focus 2 </button>
     </div>
   );
 }
